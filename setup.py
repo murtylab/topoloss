9aea634785c138e23cc4e2dad66d8ea534aa3fc8
mayukhdeb <mayukhmainak2000@gmail.com>
2024-10-06 17:35:22 -0400

    start adding real stuff

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="topoloss",
+    version="0.2.0",
+    description="topoloss",
+    author= "Mayukh Deb",
+    author_email= "mayukh@gatech.edu, mayukhmainak2000@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/toponets/topoloss",
+    packages=['topoloss'],
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
