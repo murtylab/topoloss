70e7b871aac05cbde4cbb810794b722a70b58875
mayukhdeb <mayukhmainak2000@gmail.com>
2024-10-06 19:09:09 -0400

    setup: switch to correct version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="topoloss",
-    version="0.2.0",
+    version="0.0.0",
     description="topoloss",
     author="Mayukh Deb",
     author_email="mayukh@gatech.edu, mayukhmainak2000@gmail.com",
